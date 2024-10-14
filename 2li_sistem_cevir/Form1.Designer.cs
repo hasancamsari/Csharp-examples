317b2b54000ab9d8450d769c6e79cc3c19a82120
hasancamsari <hasancmsari@gmail.com>
2024-10-14 20:05:12 +0300

    csharp examples

---
+++ 2li_sistem_cevir/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace _2li_sistem_cevir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "İKİLİ SİSTEME ÇEVİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(343, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(194, 117);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 185);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 2li_sistem_cevir/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2li_sistem_cevir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string metin;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            double sayi;
+            sayi = Convert.ToDouble(textBox1.Text);
+            int i = 1;
+            int index = 0;
+            int binyaz = 0;
+            List<int> dizi = new List<int>();
+
+            while (i <= sayi)
+            {
+                if (sayi % 2 == 0)
+                {
+                    binyaz = 0;
+                    sayi = Math.Floor((sayi / 2));
+                    index ++;
+                    dizi.Add(binyaz);
+                }
+                else
+                {
+                    sayi = Math.Floor((sayi / 2));
+                    binyaz = 1;
+                    index ++;
+                    dizi.Add(binyaz);
+                }
+            }
+
+            dizi.Reverse();
+            foreach (int chr in dizi)
+            {
+                metin += chr.ToString();
+                richTextBox1.AppendText(chr.ToString());
+            }
+
+        }
+    }
+}
